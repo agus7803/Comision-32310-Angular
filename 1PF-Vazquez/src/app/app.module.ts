3abc061d00d9c0b6c29ba22c4ff9ff92d33b5727
Agustin Vazquez <agusvazquez78@gmail.com>
2022-08-11 23:51:25 -0300

    1PF agregado

--- 1PF-Vazquez/src/app/app.module.ts
+++ 1PF-Vazquez/src/app/app.module.ts
@@ -6,17 +6,28 @@ import { ToolBarComponent } from './components/tool-bar/tool-bar.component';
 import { StudentsComponent } from './components/students/students.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppMaterialModule } from './app.material.module';
+import { EditDialogComponent } from './components/edit-dialog/edit-dialog.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AgregarDialogComponent } from './components/agregar-dialog/agregar-dialog.component';
+import { DirectivaPersonalizadaDirective } from './directivas/directiva-personalizada.directive';
+import { PipePersonalizadoPipe } from './pipes/pipe-personalizado.pipe';
+import { MatPaginator } from '@angular/material/paginator';
 
 @NgModule({
   declarations: [
     AppComponent,
     ToolBarComponent,
-    StudentsComponent
+    StudentsComponent,
+    EditDialogComponent,
+    AgregarDialogComponent,
+    DirectivaPersonalizadaDirective,
+    PipePersonalizadoPipe,
   ],
   imports: [
     BrowserModule,
     AppMaterialModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
